708b885e076593375ecee5ebc5db9e577e60d05d
SAM FAN <sam.rong.fan@googlemail.com>
2021-11-09 09:23:39 +0100

    change rabbitmq setting

--- utils/celery/settings.py
+++ utils/celery/settings.py
@@ -5,7 +5,7 @@ print(__name__ + ' loaded.')
 
 # celery -A twitter worker -l INFO
 # CELERY_BROKER_URL = 'redis://redis/2' if ((" ".join(sys.argv)).find('manage.py test') != -1) else 'redis://redis/0'
-CELERY_BROKER_URL = 'amqp://guest@localhost'
+CELERY_BROKER_URL = 'amqp://guest@rabbitmq'
 CELERY_TIMEZONE = "UTC"
 CELERY_TASK_ALWAYS_EAGER = ((" ".join(sys.argv)).find('manage.py test') != -1)
 CELERY_QUEUES = (
